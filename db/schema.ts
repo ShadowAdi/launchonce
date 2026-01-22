a4c3af780bfceccd51b28980ec35d02551c2a02b
ShadowAdi <shadowshukla76@gmail.com>
2026-01-22 20:43:04 +0530

    added user documents

--- db/schema.ts
+++ db/schema.ts
@@ -1,11 +1,26 @@
 import { sql } from "drizzle-orm";
-import { pgTable, serial, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { boolean, pgTable, serial, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-    id: uuid("id").primaryKey(),
+    id: uuid("id").primaryKey().defaultRandom(),
     name: text().notNull(),
     email: text().notNull().unique(),
     password: text().notNull(),
+    createdAt: timestamp("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: timestamp("updated_at").notNull().default(sql`CURRENT_TIMESTAMP`).$onUpdate(() => new Date())
+})
+
+export const document = pgTable("docuements", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    userId: uuid("userId").references(() => users.id).notNull(),
+    slug: varchar("slug", { length: 255 }).notNull().unique(),
+    title: varchar("title", { length: 255 }).notNull(),
+    subtitle: text("subtitle"),
+    description: text("description"),
+    coverImage: text("cover_image"),
+    content: text("content").notNull(),
+    tags: text("tags").array(),
+    visibility: varchar("visibility", { length: 10 }).notNull().default("draft"),
     createdAt: timestamp("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
     updatedAt: timestamp("updated_at").notNull().default(sql`CURRENT_TIMESTAMP`).$onUpdate(() => new Date())
 })
