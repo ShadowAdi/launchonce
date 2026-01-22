caf7fddf8d404a2263225a43c0f493ea326ad794
ShadowAdi <shadowshukla76@gmail.com>
2026-01-22 23:23:07 +0530

    quick fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-      </body>
       <Toaster/>
+      </body>
     </html>
   );
 }
